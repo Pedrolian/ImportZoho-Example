7684a22ea57c4bcf536f9f4a1072f7702833efb4
Pedrolian <Pedrodvasconcelos@gmail.com>
2020-02-04 21:37:33 -0300

    Fixing package.json

--- app.js
+++ app.js
@@ -1,4 +1,28 @@
+require('dotenv').config({path: './.env'});
+
+Logger = require('import_Zoho/utility/WinstonLogger.js');
+Logger.setPath('./log');
+
+const ZohoClass = require('import_Zoho/class/Zoho');
+const Zoho = new ZohoClass();
+
+console.time("App");
+
+// Search example
+Zoho.GetRecords("ImportacaoTeste", { "modified_since": "2020-02-03T00:00:00-03:00" } )
+.then(data => {
+
+  Logger.info(`Return: ${data.length}`);
+  console.timeEnd("App");
+
+})
+.catch(error => {
+  Logger.error(error);
+});
+
+/*
 logger = require('import_Zoho/utility/WinstonLogger.js');
 logger.setPath('./log');
 
 logger.info('test');
+*/
